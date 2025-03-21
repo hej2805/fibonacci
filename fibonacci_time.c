5cc24cd80afd5161e990be0d0f49bf3341d80db3
hej2805 <hej2805@gmail.com>
2025-03-21 18:03:09 +0900

    Update fibonacci_time.c

--- fibonacci_time.c
+++ fibonacci_time.c
@@ -13,7 +13,7 @@ int main() {
 	double time_used;
 
 	start = clock();
-	printf("fib_recursive(%d) = %lld\n", n, f(n));
+	printf("fib_recursive(%d) = %d\n", n, f(n));
 	end = clock();
 	time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 	printf("Recursive Time: %.3f sec\n", time_used);
